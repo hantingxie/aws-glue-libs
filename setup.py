1382a1f964a93c8e33c79b3e7098c8e0ea4b25c0
HantingXie <xiehanting@gmail.com>
2018-03-29 12:11:32 +0100

    Use distutils instead

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-from setuptools import find_packages, setup
+from distutils.core import setup
 
 setup(
     name='aws-glue-libs',
     version='0.0.1',
     long_description=__doc__,
-    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
 )
