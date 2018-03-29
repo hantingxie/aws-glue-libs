4fb40238aa7f7885d289b941da8233b6eb58db97
HantingXie <xiehanting@gmail.com>
2018-03-29 12:34:51 +0100

    remove the dir specification

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version='0.0.1',
     long_description=__doc__,
     packages=find_packages(where="awsglue"),
-    package_dir={"": "awsglue"},
     include_package_data=True,
     zip_safe=False,
 )
